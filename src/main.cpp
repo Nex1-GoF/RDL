5243142ef2e723743474428587292bc8406d0651
LeeJuYeon <jy122032@gmail.com>
2025-10-31 16:52:44 +0900

    feat: 소켓 생성

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "packet/HeaderPacket.hpp"
+#include "Network.hpp"
 
 int main() {
 
     std::cout << "Hello, CMake" << std::endl;
 
+    // 패킷 직렬화, 역직렬화 테스트
     HeaderPacket header('A', '1', 'B', '2', 1, 9);
     
     uint8_t buffer[9];
@@ -13,5 +15,9 @@ int main() {
     HeaderPacket new_header = header.deserialize(buffer);
     new_header.print();
 
+    // 소켓 생성 테스트 
+    Network network;
+    network.setup_sockets();
+    
     return 0;
 }
---
+++ src/network.cpp
@@ -0,0 +1,55 @@
+#include "Network.hpp"
+#include <iostream>
+
+using namespace std;
+
+int Network::create_and_bind_socket(uint16_t port, sockaddr_in& addr) {
+    int fd = create_socket();
+    set_nonblocking(fd);
+    bind_socket(fd, addr, port);
+
+    return fd;
+}
+
+int Network::create_socket() {
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+    return fd;
+}
+
+void Network::set_nonblocking(int fd) {
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (flags < 0 || fcntl(fd, F_SETFL, flags | O_NONBLOCK) < 0) {
+        perror("set non-blocking failed");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void Network::bind_socket(int fd, sockaddr_in& addr, uint16_t port) {
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(port);
+
+    if (bind(fd, reinterpret_cast<const sockaddr*>(&addr), sizeof(addr)) < 0) {
+        perror("bind failed");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void Network::setup_sockets() {
+    tx_fd        = create_and_bind_socket(9000, tx_addr);
+    cout << "TX socket created and bound to port 9000" << "\n";
+    msl_info_fd  = create_and_bind_socket(9001, msl_info_addr);
+    cout << "MSL Info socket created and bound to port 9001" << "\n";
+    msl_com_fd   = create_and_bind_socket(9002, msl_com_addr);
+    cout << "MSL Com socket created and bound to port 9002" << "\n";    
+    tgt_info_fd  = create_and_bind_socket(9003, tgt_info_addr);
+    cout << "TGT Info socket created and bound to port 9003" << "\n";   
+    src_fd       = create_and_bind_socket(9004, src_addr);
+    cout << "SRC socket created and bound to port 9004" << "\n";   
+}
